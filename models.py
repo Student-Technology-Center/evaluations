ecf9dee729976894310337e1c1a706eae40e4c2d
zefruis <pianoguy197@gmail.com>
2017-09-06 14:39:43 -0700

    Create Evaluation model

--- models.py
+++ models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Evaluation(models.Model):
+    """
+    Model representing a clients evaluation
+    """
+    instructor_name = models.CharField(max_length = 64)
+    workshop_name = models.CharField(max_length = 64)
+    workshop_date = models.DateField()
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.instructor_name + "_" + self.workshop_name
