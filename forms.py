c423392795b7cebdb8e60e745cb6275043f5f12e
zefruis <pianoguy197@gmail.com>
2017-09-06 14:40:13 -0700

    Create Evaluation form

---
+++ forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+import datetime 
+
+
+from evaluations.models import Evaluation
+
+class EvaluationForm(forms.ModelForm):
+    class Meta:
+        model = Evaluation
+        fields = ['instructor_name', 'workshop_name', 'workshop_date']
