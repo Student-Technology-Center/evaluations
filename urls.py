4128effa4c9446aa0ed631373bdd09105cc640a6
zefruis <pianoguy197@gmail.com>
2017-09-06 14:41:16 -0700

    Integrate test eval form to eval app

--- urls.py
+++ urls.py
@@ -3,5 +3,7 @@ from . import views
 from evaluations.views import evalindex
 
 urlpatterns = [
-    url(r'^$', evalindex, name='evalindex')
+    url(r'^$', evalindex, name='evalindex'),
+    url(r'^evaluate/', views.evaluate_workshop, name='evaluate-workshop'),
+    url(r'^thanks/', views.thanks, name='evaluate-thanks'),
 ]
--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -9,3 +10,32 @@ def evalindex(request):
         request,
         'evalindex.html'
     )
+
+def thanks(request):
+    return render(
+        request, 
+        'thanks.html'
+    )
+
+from .forms import EvaluationForm
+
+def evaluate_workshop(request):
+    """
+    View function for evaluating a workshop
+    """
+
+    if request.method == 'POST':
+        # if this is a POST request we need to process the form data
+        #form = EvaluateWorkshopForm(request.POST)
+        form = EvaluationForm(request.POST)
+        # check weather it's valid
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    else:
+        form = EvaluationForm()
+
+    return render(request, 'evaluate.html', {'form': form})
