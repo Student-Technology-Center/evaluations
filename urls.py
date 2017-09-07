d0d0a23acd1c3586d0bde07fb27567daa3e95ca3
zefruis <pianoguy197@gmail.com>
2017-09-06 18:22:24 -0700

    Add ability to view submittied evaluations

--- urls.py
+++ urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url, include
 from . import views
-from evaluations.views import evalindex
 
 urlpatterns = [
-    url(r'^$', evalindex, name='evalindex'),
+    url(r'^$', views.index, name='evaluate-index'),
     url(r'^evaluate/', views.evaluate_workshop, name='evaluate-workshop'),
-    url(r'^thanks/', views.thanks, name='evaluate-thanks'),
+    url(r'^view/', views.view, name='evaluate-view')
 ]
--- views.py
+++ views.py
@@ -1,41 +1,46 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.http import HttpResponseRedirect
+from .forms import *
+from .models import *
 
 # Create your views here.
 
-def evalindex(request):
-    '''Currently returns underconstruction page'''
+def index(request):
+    '''
+    Currently returns underconstruction page
+    '''
 
-    return render(
-        request,
-        'evalindex.html'
-    )
-
-def thanks(request):
-    return render(
-        request, 
-        'thanks.html'
-    )
-
-from .forms import EvaluationForm
+    return render(request, 'eval_index.html')
 
 def evaluate_workshop(request):
-    """
+    '''
     View function for evaluating a workshop
-    """
+    '''
 
     if request.method == 'POST':
         # if this is a POST request we need to process the form data
         #form = EvaluateWorkshopForm(request.POST)
         form = EvaluationForm(request.POST)
+
         # check weather it's valid
         if form.is_valid():
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            form.save()
+            return render(request, 'eval_thanks.html')
 
     else:
         form = EvaluationForm()
 
-    return render(request, 'evaluate.html', {'form': form})
+    return render(request, 'eval_evaluate.html', {'form': form})
+
+def view(request):
+    '''
+    View function for viewing workshop evaluations
+    '''
+
+    query_results = Evaluation.objects.all()
+
+    return render(request, 'eval_view.html', {'query_results': query_results})
