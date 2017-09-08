1e955322ae9e67417e61d61baaa0b736923156fb
zefruis <pianoguy197@gmail.com>
2017-09-08 13:04:03 -0700

    Add date widget to eval submission

--- forms.py
+++ forms.py
@@ -6,7 +6,13 @@ import datetime
 
 from evaluations.models import Evaluation
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class EvaluationForm(forms.ModelForm):
     class Meta:
         model = Evaluation
         fields = '__all__'
+        widgets = {
+            'workshop_date': DateInput(),
+        }
