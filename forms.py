fe1661756614f58be653af0aa2bd7282e39dedc0
zefruis <pianoguy197@gmail.com>
2017-09-16 16:50:18 -0700

    Add date picker to evaluation page

--- forms.py
+++ forms.py
@@ -6,13 +6,8 @@ import datetime
 
 from evaluations.models import Evaluation
 
-class DateInput(forms.DateInput):
-    input_type = 'date'
-
 class EvaluationForm(forms.ModelForm):
     class Meta:
         model = Evaluation
         fields = '__all__'
-        widgets = {
-            'workshop_date': DateInput(),
-        }
+        
