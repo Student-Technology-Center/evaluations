de344e22a89be9a7e78ce3b92ce37b20b3a2472d
zefruis <pianoguy197@gmail.com>
2017-10-23 18:50:37 -0700

    Add detail expander and delete from db

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='evaluate-index'),
-    url(r'^evaluate/', views.evaluate_workshop, name='evaluate-workshop'),
-    url(r'^view/', views.view, name='evaluate-view')
+    url(r'^evaluate/$', views.evaluate_workshop, name='evaluate-workshop'),
+    url(r'^view/$', views.view, name='evaluate-view'),
+    url(r'^delete/(?P<pk>\d+)/$', views.EvalDeleteView.as_view(), name='delete-eval'),
 ]
